abdc8c6f2e3138c9c31d1ae5bced9ef808333c32
Kolterg <mark.reta99@gmail.com.com>
2024-11-11 18:55:57 +0200

    Add mkDir for uploads

--- backend/src/middleware/multer.js
+++ backend/src/middleware/multer.js
@@ -1,13 +1,23 @@
 const multer = require('multer');
 const path = require('path');
+const fsp = require('fs/promises');
 
 const uploadDir = path.join(path.dirname(require.main.filename), 'uploads');
 
-const storage = multer.diskStorage({destination: 'uploads/', filename: (req, file, next) => {
-    next(null, Date.now() + path.extname(file.originalname))
-}});
+const storage = multer.diskStorage({
+    destination: async (_, __, next) => {
+        await fsp.mkdir(uploadDir, { recursive: true }).catch(e => {
+            if (e.code !== 'EEXIST') {
+                throw e;
+            }
+        });
+        next(null, uploadDir);
+    }, filename: (req, file, next) => {
+        next(null, Date.now() + path.extname(file.originalname));
+    }
+});
 
 module.exports = {
-    multer: multer({storage}),
+    multer: multer({ storage }),
     uploadDir: uploadDir
 };
